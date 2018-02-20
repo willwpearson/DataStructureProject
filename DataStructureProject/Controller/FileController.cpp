e634235369972e977916887d3f46a40eb30bd938
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-20 12:28:58 -0700

    Added the read data to list implementation.

--- DataStructureProject/Controller/FileController.cpp
+++ DataStructureProject/Controller/FileController.cpp
@@ -45,3 +45,42 @@ vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
     
     return crimeVector;
 }
+
+LinkedList<CrimeData> FileController :: readDataToList(string fileName)
+{
+    LinkedList<CrimeData> crimes;
+    
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(fileName);
+    
+    //If the file exists at that path.
+    if(dataFile.is_open())
+    {
+        //Keep reading until you are at the end of the file.
+        while(!dataFile.eof())
+        {
+            //Grab each line from the CSV separated by the carriage return character.
+            getline(dataFile, currentCSVLine, '\r');
+            //Exclude header row
+            if(rowCount != 0)
+            {
+                //Create a CrimeData instance from the line. Exclude a blank line (usually if opened separately)
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimes.add(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimes;
+}
