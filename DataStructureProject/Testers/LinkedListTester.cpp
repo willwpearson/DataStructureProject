4a3cca9062bad99591d994f38efd7ee057a7cba7
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-20 13:20:01 -0700

    Added the implementation of linked list for ints and the CrimeData.

--- DataStructureProject/Testers/LinkedListTester.cpp
+++ DataStructureProject/Testers/LinkedListTester.cpp
@@ -10,6 +10,51 @@
 
 void LinkedListTester :: testListBasics()
 {
-    LinkedList<int> numbesrs;
+    LinkedList<int> numbers;
     numbers.add(9);
+    cout << numbers.getFront()->getData() << " Is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " Is at the end of the list and should be 9" << endl;
+    
+    numbers.add(0);
+    cout << numbers.getFront()->getData() << " Is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " is at the end of the list and should be 0" << endl;
+    
+    numbers.addAtIndex(1,2);
+    numbers.add(324);
+    cout << numbers.getFront()->getData() << " Is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " is at the end of the list and should be 324" << endl;
+    
+    cout << "This loop should print 4 lines" << endl;
+    for(int index = 0; index < numbers.getSize(); index++)
+    {
+        cout << numbers.getFromIndex(index) << endl;
+    }
+    
+    numbers.remove(0);
+    numbers.add(32567);
+    numbers.addAtIndex(0,2312);
+    
+    cout << numbers.getFront()->getData() << " Is at the front of the list and should be 2312" << endl;
+    cout << numbers.getEnd()->getData() << " is at the ned of the list and should be 32567" << endl;
+}
+
+void LinkedListTester :: testListWithData()
+{
+    Timer listTimer;
+    
+    listTimer.startTimer();
+    LinkedList<CrimeData> crimes = FileController :: readDataToList("/Users/wpea1865/Documents/C++ Projects/DataStructureProject/DataStructureProject/Data/crime.csv");
+    listTimer.stopTimer();
+    cout << "This is how long it took to read the structure into our custom data structure" << endl;
+    listTimer.displayInformation();
+    
+    listTimer.resetTimer();
+    cout << "Here is how long it takes to access a random data value" << endl;
+    listTimer.startTimer();
+    int randomLocation = (rand() * rand()) % crimes.getSize();
+    cout << "The random index is " << randomLocation << endl;
+    double totalViolentRate = crimes.getFromIndex(randomLocation).getAllViolentRates();
+    listTimer.stopTimer();
+    cout << "The random crime stat is: " << totalViolentRate << " , and here is the time" << endl;
+    listTimer.displayInformation();
 }
