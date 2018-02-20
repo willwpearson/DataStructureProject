4ed7338d0466a76d2ddf0c9107af9b71736d8758
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-20 12:32:04 -0700

    Added the linked list tester files and the header declaration.

---
+++ DataStructureProject/Testers/LinkedListTester.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedListTester.cpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 2/20/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "LinkedListTester.hpp"
---
+++ DataStructureProject/Testers/LinkedListTester.hpp
@@ -0,0 +1,25 @@
+//
+//  LinkedListTester.hpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 2/20/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinkedListTester_hpp
+#define LinkedListTester_hpp
+
+#include "../Model/Structures/Linear/LinkedList.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+#include "../Data/CrimeData.hpp"
+#include <iostream>
+
+class LinkedListTester
+{
+public:
+    void testListBasics();
+    void testListWithData();
+};
+
+#endif /* LinkedListTester_hpp */
