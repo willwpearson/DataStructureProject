3de6c39ae568696aa9b8529ad6f1e3e123567e1a
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-20 12:21:37 -0700

    Added the include and the declaration of the method.

--- DataStructureProject/Controller/FileController.hpp
+++ DataStructureProject/Controller/FileController.hpp
@@ -10,6 +10,7 @@
 #define FileController_hpp
 
 #include "../Data/CrimeData.hpp"
+#include "../Model/Structures/Linear/LinkedList.hpp"
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -22,6 +23,7 @@ class FileController
 {
 public:
     static vector<CrimeData> readCrimeDataToVector(string filename);
+    static LinkedList<CrimeData> readDataToList(string filename);
 };
 
 #endif /* FileController_hpp */
