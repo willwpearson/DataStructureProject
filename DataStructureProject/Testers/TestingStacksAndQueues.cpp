3dee7fba6076520a86492f75eefdb378b083ad61
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-26 12:14:02 -0700

    Created a new class for testing stacks and queues.

---
+++ DataStructureProject/Testers/TestingStacksAndQueues.cpp
@@ -0,0 +1,9 @@
+//
+//  TestingStacksAndQueues.cpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "TestingStacksAndQueues.hpp"
---
+++ DataStructureProject/Testers/TestingStacksAndQueues.hpp
@@ -0,0 +1,23 @@
+//
+//  TestingStacksAndQueues.hpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef TestingStacksAndQueues_hpp
+#define TestingStacksAndQueues_hpp
+
+#include "../Controller/Tools/Timer.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Model/Structures/Linear/Queue.hpp"
+#include "../Model/Structures/Linear/Stack.hpp"
+#include "../Model/Structures/Linear/List.hpp"
+#include <iostream>
+
+using namespace std;
+
+
+
+#endif /* TestingStacksAndQueues_hpp */
