58659b03911415cb236bd5f3547b051dcf96e89b
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-26 12:36:33 -0700

    Added the stack versus queue implementation.

--- DataStructureProject/Testers/TestingStacksAndQueues.cpp
+++ DataStructureProject/Testers/TestingStacksAndQueues.cpp
@@ -66,7 +66,48 @@ void TestingStacksAndQueues :: stackVersusList()
 
 void TestingStacksAndQueues :: stackVersusQueue()
 {
+    vector<CrimeData> data = FileController :: readCrimeDataToVector("/Users/wpea1865/Documents/C++ Projects/DataStructureProject/DataStructureProject/Data/crime.csv");
     
+    Stack<CrimeData> crimeStack;
+    Queue<CrimeData> crimeQueue;
+    
+    for(int index = 0; index < 100; index ++)
+    {
+        CrimeData temp = data.at(index);
+        crimeStack.push(temp);
+        crimeQueue.enqueue(temp);
+    }
+    
+    Queue<int> queue;
+    Stack<int> stack;
+    Queue<int> queueFromStack;
+    Stack<int> stackFromQueue;
+    
+    for(int index = 0; index < 10; index ++)
+    {
+        queue.enqueue(index);
+        stack.push(index);
+    }
+    
+    for(int index = 0; index < 10; index ++)
+    {
+        cout << index << endl;
+        cout << "Queue result: " << queue.dequeue() << endl;
+        cout << "Stack result: " << stack.pop() << endl;
+    }
+    
+    for(int index = 0; index < 10; index ++)
+    {
+        queue.enqueue(index);
+        stack.push(index);
+    }
+    
+    for(int index = 0; index < 10; index ++)
+    {
+        cout << index << endl;
+        cout << "Stack result: " << stackFromQueue.pop() << endl;
+        cout << "Queue result: " << queueFromStack.dequeue() << endl;
+    }
 }
 
 void TestingStacksAndQueues :: queueVersusList()
