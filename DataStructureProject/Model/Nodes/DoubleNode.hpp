124dcbc6c409783b59fef2c0f55a218dc7b8f5a8
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-26 11:52:55 -0700

    Made the new Double Node class.

---
+++ DataStructureProject/Model/Nodes/DoubleNode.hpp
@@ -0,0 +1,13 @@
+//
+//  DoubleNode.hpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef DoubleNode_hpp
+#define DoubleNode_hpp
+
+
+#endif /* DoubleNode_hpp */
