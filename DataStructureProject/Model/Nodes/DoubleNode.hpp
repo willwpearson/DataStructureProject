af7defd5ab3c16d21187a8d7138742efa9ea1303
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-26 11:56:58 -0700

    Added the class declaration for double node.

--- DataStructureProject/Model/Nodes/DoubleNode.hpp
+++ DataStructureProject/Model/Nodes/DoubleNode.hpp
@@ -6,8 +6,41 @@
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
+#include "Node.hpp"
+
 #ifndef DoubleNode_hpp
 #define DoubleNode_hpp
+
+template <class Type>
+class DoubleNode : public Node<Type>
+{
+protected:
+    DoubleNode<Type> * previous;
+    DoubleNode<Type> * next;
+public:
+    DoubleNode();
+    DoubleNode(Type data);
+    DoubleNode(Type data, DoubleNode<Type> * previous, DoubleNode<Type> * next);
+    
+    DoubleNode<Type> * getPrevious();
+    DoubleNode<Type> * getNext();
+    
+    void setPrevious(DoubleNode<Type> * previous);
+    void setNext(DoubleNode<Type> * next);
+};
+
+template <class Type>
+DoubleNode<Type> :: DoubleNode() : Node<Type>()
+{
+    previous = nullptr;
+    next = nullptr;
+}
+
+template <class Type>
+DoubleNode<Type> :: DoubleNode(Type data) : Node<Type>(data)
+{
+    
+}
 
 
 #endif /* DoubleNode_hpp */
