2d06159b462d9f82558fc39ec93fe3368688cf7b
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-05 13:01:07 -0700

    Added the code for recursion with words.

--- DataStructureProject/Testers/RecursionTester.cpp
+++ DataStructureProject/Testers/RecursionTester.cpp
@@ -12,17 +12,27 @@ int RecursionTester :: fib(int number)
 {
     if(number == 0 || number == 1)
     {
+        cout << "Reached a base case" << endl;
         return 1;
     }
     else
     {
+        cout << "Reached a recursive case with: " << number - 1 << "and" << number - 2 << endl;
         return fib(number - 1) + fib (number - 2);
     }
 }
 
 string RecursionTester :: mystery(string word)
 {
-    return "";
+    if (word.length() == 1)
+    {
+        return word;
+    }
+    else
+    {
+        
+        return word + mystery(word.substr(0, word.length() - 1));
+    }
 }
 
 void RecursionTester :: testRecursionNumbers()
@@ -37,5 +47,7 @@ void RecursionTester :: testRecursionNumbers()
 
 void RecursionTester :: testRecursionString()
 {
-    
+    cout << mystery("X") << endl;
+    cout << mystery("car") << endl;
+    cout << mystery("apple") << endl;
 }
