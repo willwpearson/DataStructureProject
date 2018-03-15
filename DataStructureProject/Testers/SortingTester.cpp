748e3a070533545cef81952f025c66402e7dd490
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-15 11:50:11 -0600

    Added the sorting testing class and the header file code.

---
+++ DataStructureProject/Testers/SortingTester.cpp
@@ -0,0 +1,9 @@
+//
+//  SortingTester.cpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 3/15/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "SortingTester.hpp"
---
+++ DataStructureProject/Testers/SortingTester.hpp
@@ -0,0 +1,27 @@
+//
+//  SortingTester.hpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 3/15/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef SortingTester_hpp
+#define SortingTester_hpp
+
+#include <iostream>
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+
+class SortingTester
+{
+private:
+    int swapCount;
+    void quickSort(vector<CrimeData> & data, int low, int high);
+    int partition(vector<CrimeData> & data, int low, int high);
+    void swap(vector<CrimeData> & info, int small, int large);
+public:
+    void testSorts();
+};
+
+#endif /* SortingTester_hpp */
