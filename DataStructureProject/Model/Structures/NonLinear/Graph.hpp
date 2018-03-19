2b955033800d698676b51de06763ab061c39a507
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-19 12:22:20 -0600

    Added the graph data structure class declaration.

---
+++ DataStructureProject/Model/Structures/NonLinear/Graph.hpp
@@ -0,0 +1,60 @@
+//
+//  Graph.hpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 3/19/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 20;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph()
+    
+    //Graph Operations
+    void addVertex(const Type& value);
+    
+    //Connect vertices
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target);
+    //Disconnect vertices
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //Accessors
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    //Check connections
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target) const;
+    std::set<int> neighbors(int vertex) const;
+    
+    //Traversals
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+};
+
+
+
+#endif /* Graph_hpp */
