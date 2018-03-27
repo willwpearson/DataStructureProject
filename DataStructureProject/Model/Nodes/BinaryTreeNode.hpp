857bb4a39ac9a445ded6276df141cb43af040ec0
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-27 12:12:47 -0600

    Wrote the class definition for Binary Tree.

--- DataStructureProject/Model/Nodes/BinaryTreeNode.hpp
+++ DataStructureProject/Model/Nodes/BinaryTreeNode.hpp
@@ -9,7 +9,36 @@
 #ifndef BinaryTreeNode_hpp
 #define BinaryTreeNode_hpp
 
+#include "Node.hpp"
 
+template<class Type>
+class BinaryTreeNode : public Node<Type>
+{
+private:
+    BinaryTreeNode<Type> * root;
+    BinaryTreeNode<Type> * left;
+    BinaryTreeNode<Type> * right;
+public:
+    BinaryTreeNode();
+    BinaryTreeNode(Type data);
+    
+    BinaryTreeNode<Type> * getRoot();
+    BinaryTreeNode<Type> * getLeft();
+    BinaryTreeNode<Type> * getRight();
+    
+    void setRootNode(BinaryTreeNode<Type> * root);
+    void setLeftNode(BinaryTreeNode<Type> * left);
+    void setRightNode(BinaryTreeNode<Type> * right);
+};
 
-
+template<class Type>
+template<class Type>
+template<class Type>
+template<class Type>
+template<class Type>
+template<class Type>
+template<class Type>
+template<class Type>
+template<class Type>
+template<class Type>
 #endif /* BinaryTreeNode_hpp */
