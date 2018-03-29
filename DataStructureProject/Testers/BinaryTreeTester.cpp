dc50c3c11caadffc7d1d239a38d88308ae4218e4
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-29 12:54:09 -0600

    Added the tester for binary search tree.

--- DataStructureProject/Testers/BinaryTreeTester.cpp
+++ DataStructureProject/Testers/BinaryTreeTester.cpp
@@ -7,3 +7,22 @@
 //
 
 #include "BinaryTreeTester.hpp"
+
+void BinaryTreeTester :: doTreeStuff()
+{
+    testTree.insert(3);
+    testTree.insert(1);
+    testTree.insert(2);
+    testTree.insert(-3246000);
+    testTree.insert(-12);
+    testTree.insert(7);
+    testTree.insert(5);
+    testTree.insert(6);
+    testTree.insert(8);
+    testTree.insert(5000);
+    testTree.insert(9001);
+    testTree.insert(10);
+    testTree.insert(600);
+    
+    testTree.inOrderTraversal();
+}
