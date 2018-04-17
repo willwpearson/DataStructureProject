b8595981e835f66b4b0ebbe0b77880a81b09319b
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-17 12:08:04 -0600

    Added the basic right and left rotations.

--- DataStructureProject/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructureProject/Model/Structures/NonLinear/AVLTree.hpp
@@ -48,8 +48,29 @@ int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node)
 }
 
 template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: leftRotation(BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent->getLeft();
+    
+    parent->setLeftChild(changedNode->getRight());
+    
+    changedNode->setRightChild(parent);
+    
+    return changedNode;
+}
 
-
-
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: rightRotation(BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent->getRight();
+    
+    parent->setRightChild(changedNode->getLeft());
+    
+    changedNode->setLeftChild(parent);
+    
+    return changedNode;
+}
 
 #endif /* AVLTree_hpp */
