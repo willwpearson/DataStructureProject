ee18ee922acfeb266613d88ec2d754270fdd8fe6
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-17 11:53:18 -0600

    Added the class declaration for the AVL Tree.

---
+++ DataStructureProject/Model/Structures/NonLinear/AVLTree.hpp
@@ -0,0 +1,36 @@
+//
+//  AVLTree.hpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 4/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+#include "BinarySearchTree.hpp"
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+};
+
+
+#endif /* AVLTree_hpp */
