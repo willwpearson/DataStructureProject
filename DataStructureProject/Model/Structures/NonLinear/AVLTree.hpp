678d209b134dcbd3951b8b272fc904f2a890e519
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-17 12:01:52 -0600

    Added the method for the height differences.

--- DataStructureProject/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructureProject/Model/Structures/NonLinear/AVLTree.hpp
@@ -25,12 +25,31 @@ private:
     BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
     BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
     
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
 public:
     AVLTree();
     
     void insert(Type itemToInsert);
     void remove(Type value);
 };
+
+/*
+ A Negative value means the right is greater than the left
+ */
+template <class Type>
+int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node)
+{
+    int balance;
+    int leftHeight = this->calculateHeight(node->getLeftNode());
+    int rightHeight = this->calculateHeight(node->getRightNode());
+    balance = leftHeight - rightHeight;
+    return balance;
+}
+
+template <class Type>
+
+
 
 
 #endif /* AVLTree_hpp */
