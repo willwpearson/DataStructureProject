91907446be474f45e129eca45b401e161cb202ef
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-23 11:48:02 -0600

    Added the methods for the N_Ary Tree node class.

--- DataStructureProject/Model/Nodes/N_AryTreeNode.hpp
+++ DataStructureProject/Model/Nodes/N_AryTreeNode.hpp
@@ -32,4 +32,56 @@ public:
     void addChild(Type data);
 };
 
+template <class Type>
+N_AryTreeNode<Type> :: N_AryTreeNode()
+{
+    //Unused
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: N_AryTreeNode(Type data) : Node<Type>(data)
+{
+    
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: ~N_AryTreeNode()
+{
+    for(int index = nodes.size() - 1; index >= 0; index --)
+    {
+        delete nodes[index];
+    }
+}
+
+template <class Type>
+N_AryTreeNode<Type> * N_AryTreeNode<Type> :: getRoot()
+{
+    return root;
+}
+
+template <class Type>
+vector<N_AryTreeNode<Type>*> N_AryTreeNode<Type> :: getNodes()
+{
+    return nodes;
+}
+
+template <class Type>
+int N_AryTreeNode<Type> :: getChildCount()
+{
+    return nodes.size();
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: setRoot(N_AryTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: addChild(Type data)
+{
+    N_AryTreeNode<Type> * node = new N_AryTreeNode<Type>(data);
+    nodes.push_back(node);
+}
+
 #endif /* N_AryTreeNode_hpp */
