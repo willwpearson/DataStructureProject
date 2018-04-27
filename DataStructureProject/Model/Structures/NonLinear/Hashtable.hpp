8d140d16bab37225fece2afc2378761bb5f36634
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-27 12:22:34 -0600

    Added the hashtable class and the definition section for the hash table class.

---
+++ DataStructureProject/Model/Structures/NonLinear/Hashtable.hpp
@@ -0,0 +1,38 @@
+//
+//  Hashtable.hpp
+//  DataStructureProject
+//
+//  Created by Pearson, William on 4/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Hashtable_hpp
+#define Hashtable_hpp
+
+#include "../Nodes/HashNode.hpp"
+
+template <class Type>
+class Hashtable
+{
+private:
+    HashNode<Type> ** internalStorage;
+    long size;
+    long capacity;
+    double loadFactor;
+    
+    long getNextPrime();
+    bool isPrime(long current);
+    void resize();
+    long findPosition(HashNode<Type> * insertedNode);
+    long handleCollision(HashNode<Type> * insertedNode, long index);
+    
+public:
+    Hashtable();
+    ~Hashtable();
+    
+    void insert(Type data);
+    long getSize();
+};
+
+
+#endif /* Hashtable_hpp */
