83c99fefaabe20b9067b01c1c5bf70130e83049c
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-05-07 11:41:06 -0600

    Added the method to retrieve values and to check if value is in table.

--- DataStructureProject/Model/Structures/NonLinear/Hashtable.hpp
+++ DataStructureProject/Model/Structures/NonLinear/Hashtable.hpp
@@ -190,4 +190,27 @@ void Hashtable<Type> :: insert(Type value)
     }
 }
 
+template <class Type>
+HashNode<Type> * Hashtable<Type> :: get(long index)
+{
+    assert(index < capacity);
+    return internalStorage[index];
+}
+
+template <class Type>
+bool Hashtable<Type> :: contains(HashNode<Type> * value)
+{
+    if(internalStorage[findPosition(value)]->getData() == value->getData())
+    {
+        return true;
+    }
+    
+    long other = handleCollision(findPosition(value));
+    if(internalStorage[other]->getData() == value->getData())
+    {
+        return true;
+    }
+    return false;
+}
+
 #endif /* Hashtable_hpp */
