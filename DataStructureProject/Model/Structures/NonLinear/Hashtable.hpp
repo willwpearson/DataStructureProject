8fc1429ceefd524ad7a9c37c07c2817cf0baa8e6
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-05-07 11:36:55 -0600

    Added the methods to check if its prime and to get the prime value.

--- DataStructureProject/Model/Structures/NonLinear/Hashtable.hpp
+++ DataStructureProject/Model/Structures/NonLinear/Hashtable.hpp
@@ -52,13 +52,35 @@ Hashtable<Type> :: ~Hashtable()
 template <class Type>
 bool Hashtable<Type> :: isPrime(long current)
 {
-    return false;
+    if(current <= 1)
+    {
+        return false;
+    }
+    else if(current == 2 || current == 3)
+    {
+        return true
+    }
+    else if(current % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for(int next = 3; next <= sqrt(current) + 1; next += 2)
+        {
+            if(current % next == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 template <class Type>
 long Hashtable<Type> :: findPosition(HashNode<Type> * insert)
 {
-    long insertPosition = insert->getKey() %  this->capacity;
+    long insertPosition = insert->getKey() % this->capacity;
     return insertPosition;
 }
 
@@ -92,7 +114,14 @@ long Hashtable<Type> :: getSize()
 template <class Type>
 long Hashtable<Type> :: getNextPrime()
 {
-    return -1;
+    long nextPrime = (this->capacity * 2) + 1;
+    
+    while(!isPrime(nextPrime))
+    {
+        nextPrime += 2;
+    }
+    
+    return nextPrime;
 }
 
 
